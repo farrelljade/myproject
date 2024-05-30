ff2a5d0bfde2e4063b3ca1b0b35e4eb803f75769
Mikel Farrell <farrellmikel@yahoo.com>
2024-05-30 22:58:12 +0100

    Update kernel file

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use App\Console\Commands\GenerateOrders;
 use App\Console\Commands\GenerateCustomers;
+use App\Console\Commands\GenerateUsers;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -17,6 +18,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         GenerateOrders::class,
         GenerateCustomers::class,
+        GenerateUsers::class,
     ];
 
     /**
